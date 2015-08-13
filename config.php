ab1f2c99ebe0c8019de0b0b5c238334f98eb0431
rjprieto <ramonj.prieto.exts@juntadeandalucia.es>
2015-08-13 14:41:33 +0200

    Configuracion

--- config.php
+++ config.php
@@ -13,17 +13,15 @@
 	//define("DB_TYPE", 'MYSQL');
 	define("DB_TYPE", 'POSTGRES');
 	
-	/*define("DB_HOST", "ec2-54-235-147-211.compute-1.amazonaws.com"); 
+	define("DB_HOST", "ec2-54-235-147-211.compute-1.amazonaws.com"); 
 	define("DB_USER", "lqpvjhwpklpgnc"); 
 	define("DB_PASSWORD", "JZLUIQgYzNuyLx6FMR8Gl3PGLZ"); 
-	define("DB_DATABASE", "d5609at5dvtl0q"); */
+	define("DB_DATABASE", "d5609at5dvtl0q");
 	
-	define("DB_HOST", "localhost"); 
+	/*define("DB_HOST", "localhost"); 
 	define("DB_USER", "postgres"); 
 	define("DB_PASSWORD", "longhorn"); 
-	define("DB_DATABASE", "gcm");
-	
-	//define("DB_URL", "postgres://lqpvjhwpklpgnc:JZLUIQgYzNuyLx6FMR8Gl3PGLZ@ec2-54-235-147-211.compute-1.amazonaws.com:5432/d5609at5dvtl0q");
+	define("DB_DATABASE", "gcm");*/
 	
 	/* 
 	* Google API Key 
